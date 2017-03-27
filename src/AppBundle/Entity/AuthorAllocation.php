dd9abbd6dec7d97c126202810a8c6ecacd9b4887
Kausha Keraminiyage <keraminiyage@yahoo.co.uk>
2017-03-27 14:11:40 +0100

    New entities created / updated

--- src/AppBundle/Entity/AuthorAllocation.php
+++ src/AppBundle/Entity/AuthorAllocation.php
@@ -3,20 +3,13 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-<<<<<<< HEAD
-=======
 use Doctrine\Common\Collections\ArrayCollection;
->>>>>>> origin/master
 
 /**
  * Class AuthorAllocation
  * @package AppBundle\Entity
  * @ORM\Entity
-<<<<<<< HEAD
  * @ORM\Table(name="author_paper_allocations")
-=======
- * @ORM\Table(name="author_allocations")
->>>>>>> origin/master
  * @ORM\Entity(repositoryClass="AppBundle\Repository\AuthorAllocationRepository")
  */
 class AuthorAllocation {
@@ -25,39 +18,24 @@ class AuthorAllocation {
      *
      * @var
      * @ORM\Id
-<<<<<<< HEAD
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Paper", inversedBy="authorAllocations")
      * @ORM\JoinColumn(name="paper", referencedColumnName="id")
      *
      */
     protected $paper;
-=======
-     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Author", inversedBy="paperAllocations")
-     * @ORM\JoinColumn(name="author", referencedColumnName="id")
-     */
-    protected $author;
->>>>>>> origin/master
 
     /**
      *
      * @var
      * @ORM\Id
-<<<<<<< HEAD
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Author", inversedBy="paperAllocations")
      * @ORM\JoinColumn(name="author", referencedColumnName="id")
      */
     protected $author;
-=======
-     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Paper", inversedBy="authorAllocations")
-     * @ORM\JoinColumn(name="paper", referencedColumnName="id")
-     */
-    protected $paper;
->>>>>>> origin/master
 
     /**
      * @return mixed
      */
-<<<<<<< HEAD
     public function getPaper()
     {
         return $this->paper;
@@ -69,25 +47,11 @@ class AuthorAllocation {
     public function setPaper($paper)
     {
         $this->paper = $paper;
-=======
-    public function getAuthor()
-    {
-        return $this->author;
-    }
-
-    /**
-     * @param mixed $author
-     */
-    public function setAuthor($author)
-    {
-        $this->author = $author;
->>>>>>> origin/master
     }
 
     /**
      * @return mixed
      */
-<<<<<<< HEAD
     public function getAuthor()
     {
         return $this->author;
@@ -99,19 +63,6 @@ class AuthorAllocation {
     public function setAuthor($author)
     {
         $this->author = $author;
-=======
-    public function getPaper()
-    {
-        return $this->paper;
-    }
-
-    /**
-     * @param mixed $paper
-     */
-    public function setPaper($paper)
-    {
-        $this->paper = $paper;
->>>>>>> origin/master
     }
 
 }
